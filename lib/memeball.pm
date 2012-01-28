26cbaa06b83569eb31e3c0ee1094dfe62850b760
Yanick Champoux <yanick@babyl.dyndns.org>
2012-01-28 10:23:52 -0500

    It's alive

--- lib/memeball.pm
+++ lib/memeball.pm
@@ -1,10 +1,39 @@
 package memeball;
 use Dancer ':syntax';
 
-our $VERSION = '0.1';
+use FindBin;
+use Path::Class qw/ dir file /;
+
+our $VERSION = '0.0.1';
+
+my %answers = %{ config->{answers} };
+
+# sanity check
+debug "loading possible answers";
+for my $filename ( keys %answers ) {
+    debug "\t$answers{$filename}";
+    next if -f file( $FindBin::Bin, '..', 'public', 'answers', $filename );
+    debug "\tfile $filename not found, skipping answer";
+
+    delete $answers{$filename};
+}
 
 get '/' => sub {
     template 'index';
+};
+
+post '/ask' => sub {
+
+    my $question = param('question');
+    my @files = keys %answers;
+    my $file = $files[rand @files];
+
+    template answer => {
+        question => param('question'),
+        answer => $answers{$file},
+        pic => $file,
+    };
+
 };
 
 true;
